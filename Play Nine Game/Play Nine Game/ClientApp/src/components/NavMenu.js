3c182438ea4183df4921144dd4728ef21e770408
Shaf Hassan <sharfarazhassan@gmail.com>
2018-11-21 12:20:32 -0500

    Play Nine Game UI

---
+++ Play Nine Game/Play Nine Game/ClientApp/src/components/NavMenu.js
@@ -0,0 +1,47 @@
+﻿import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import './NavMenu.css';
+import './App.scss';
+
+export class NavMenu extends Component {
+  displayName = NavMenu.name
+
+  render() {
+    return (
+      <Navbar inverse fixedTop fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to={'/'}>Play_Nine_Game</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <LinkContainer to={'/'} exact>
+              <NavItem>
+                <Glyphicon glyph='home' /> Home
+              </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/counter'}>
+              <NavItem>
+                <Glyphicon glyph='education' /> Counter
+              </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/fetchdata'}>
+              <NavItem>
+                <Glyphicon glyph='th-list' /> Fetch data
+              </NavItem>
+            </LinkContainer>
+            <LinkContainer to={'/playnine'}>
+              <NavItem>
+                <Glyphicon glyph='th-list' /> Play Nine Game
+              </NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
---
+++ Play Nine Game/Play Nine Game/ClientApp/src/components/PlayNine.js
@@ -0,0 +1,91 @@
+﻿import React from 'react';
+
+
+export class Stars extends React.Component {
+    state = {}
+    render() {
+        return (
+            <div>
+            <span className="col-5">
+                <span className="stars">+</span>
+                <span className="stars">+</span>
+                <span className="stars">+</span>
+                <span className="stars">+</span>
+               </span>
+            </div>    
+        )
+    }
+}
+
+export class NumbersList extends React.Component {
+    state = { num_list : [1, 2, 3, 4, 5, 6, 7, 8, 9] }
+    render() {
+        return (
+            <div className="card text-center">
+                <div>
+                    <span className="numbersStyle">1</span>
+                    <span className="numbersStyle">2</span>
+                    <span className="numbersStyle">3</span>
+                </div>
+            </div>
+        )
+    }
+}
+
+export class Answer extends React.Component {
+    render() {
+        return (
+            <div className="col-5"><span>Answer component</span></div>
+        )
+    }
+}
+
+export class Checker extends React.Component {
+    state = {}
+    render() {
+        return (
+            <div className="col-2 center">
+                <center><button> = </button></center>
+            </div>
+        )
+    }
+}
+
+export class Redraw extends React.Component {
+    state = {}
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export class GameBoard extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Play Nine: </h1>
+                <hr />
+                <div className="row">
+                    <Stars />
+                    <Checker />
+                    <Answer />
+                </div>
+                <br />
+                <NumbersList />
+            </div>
+        )
+    }
+}
+
+export class PlayNine extends React.Component {
+    state = { list: [] }
+
+    render() {
+        return (
+            <div>
+                <GameBoard />
+            </div>
+        )
+    }
+}
