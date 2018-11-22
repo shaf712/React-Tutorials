dba1e342cb41669a19f413e5f9dcced38c3ff3bf
Shaf <sharfarazhassan@gmail.com>
2018-11-22 18:37:12 -0500

    Play Nine  game completion

--- Play Nine Game/Play Nine Game/ClientApp/src/components/PlayNine.js
+++ Play Nine Game/Play Nine Game/ClientApp/src/components/PlayNine.js
@@ -2,9 +2,8 @@
 
 
 const Stars = (props) => {
-    const NumOfStars = 1+ Math.floor(Math.random() * 9); 
     let stars = [] 
-    for (let i = 0; i < NumOfStars; i++)
+    for (let i = 0; i < props.randomNumberOfStars; i++)
     {
         stars.push(<span key={i} className="stars">+</span>)
     }
@@ -20,11 +19,24 @@ const Stars = (props) => {
 const NumbersList = (props) => {
     const numberList = []
 
-    //Functional components such as these CANNOT have methods 
+    const numberClassName = (number) =>
+    {
+        if (props.selectedNumbers.indexOf(number) >= 0) {
+            return 'numbersStyle selected';
+        }
+
+        else if (props.usedNumbers.indexOf(number) >= 0) {
+            return 'numbersStyle used';
+        }
+
+        else {
+            return 'numbersStyle';
+        }
+    }
 
     for (let i = 1; i < 10; i++)
     {
-        numberList.push(<span className="numbersStyle">{i}</span>); 
+        numberList.push(<span className={numberClassName(i)} onClick={() => props.appendToList(i)} >{i}</span>); 
     }
 
         return (
@@ -36,40 +48,103 @@ const NumbersList = (props) => {
         )
 }
 
-export class Answer extends React.Component {
-    render() {
+const Answer = (props) => {
+
         return (
-            <div className="col-5"><span>Answer component</span></div>
+            <div className="col-5">
+                {props.passValues.map((num, i) => <span onClick={() => props.putBackNumber(num)} className="numbersStyle" key={i}>{num}</span>)}
+            </div>
         )
-    }
 }
 
 const Checker = (props) => {
 
         return (
             <div className="col-2 center">
-                <center><button> = </button></center>
+                <center><button onClick={props.checker}> = </button></center>
             </div>
         )
 }
 
-export class Redraw extends React.Component {
-    state = {}
-    render() {
-        return (
-            <div></div>
-        )
+
+const Restart = (props) => {
+
+    return (
+        <div className="col-2 center">
+            <center><button onClick={props.restartGame} className="redraw"> RESTART GAME </button></center>
+        </div>
+    )
+}
+
+const Redraw = (props) => {
+
+    const redrawClassName = (limit) => {
+        if (limit != 0) {
+            return 'redraw'; 
+        }
+        else {
+            return 'redraw limit'; 
+        }
     }
+        return (
+            <div><center className={redrawClassName(props.refreshLimit)}><button onClick={props.redrawStars}>Redraw ({props.refreshLimit})</button></center></div>
+        )
 }
 
 export class GameBoard extends React.Component {
-    state = { selectedNumbers: [] };
-    //TASK 1: Figure out how to push a number to this array on click 
-    //TASK 2: Figure out what the UI will do based on the numbers stored in this array
-    //
+    //if you have many state components, you can wrap  them in a function and assign the function to "state" 
 
-    ShowAlert = () => {
-        console.log('alert')
+    static InitialState = () => ({
+        selectedNumbers: [], usedNumbers: [], randomNumberOfStars: 1 + Math.floor(Math.random() * 9), refreshLimit: 5
+    })
+
+    state = GameBoard.InitialState();
+
+    redraw = () => {
+        console.log('redrawing!')
+        this.setState(previousState => ({
+            randomNumberOfStars: 1 + Math.floor(Math.random() * 9), 
+            refreshLimit: previousState.refreshLimit - 1
+        }))
+    }
+
+    appendToList = (num) => {
+        console.log('heres the num TO ADD AS SELECTED', num)
+        this.setState(previousState => ({
+            selectedNumbers: previousState.selectedNumbers.concat(num)
+        }))
+    }
+
+    putBackNumber = (num) => {
+        console.log('heres the num TO PUT BACK', num)
+        this.setState(previousState => ({
+            selectedNumbers: previousState.selectedNumbers.filter(function (v, index) { return v != num })
+        }))
+    }
+
+    checker = () => {
+        let sum = 0; 
+
+        for (let i = 0; i < this.state.selectedNumbers.length; i++) {
+                sum = sum + this.state.selectedNumbers[i]
+        }
+
+        if (sum == this.state.randomNumberOfStars) {
+            this.setState(previousState => ({
+                randomNumberOfStars: 1 + Math.floor(Math.random() * 9)
+            }))
+            this.setState(previousState => ({
+                usedNumbers: previousState.usedNumbers.concat(previousState.selectedNumbers), 
+                selectedNumbers: [] 
+            }))
+        }
+        else {
+            console.log('INCORRECT');
+        }
+    }
+
+    restartGame = () => {
+        this.setState(GameBoard.InitialState())
     }
 
     render() {
@@ -78,19 +153,21 @@ export class GameBoard extends React.Component {
                 <h1>Play Nine: </h1>
                 <hr />
                 <div className="row">
-                    <Stars />
-                    <Checker />
-                    <Answer />
+                    <Stars randomNumberOfStars={this.state.randomNumberOfStars} />
+                    <Checker checker={this.checker} />
+                    <Redraw refreshLimit={this.state.refreshLimit} redrawStars={this.redraw} />
+                    <Answer indicator={this.state.indicator} putBackNumber={this.putBackNumber} passValues={this.state.selectedNumbers} />
                 </div>
                 <br />
-                <NumbersList />
+                <NumbersList usedNumbers={this.state.usedNumbers} selectedNumbers={this.state.selectedNumbers} appendToList={this.appendToList} />
+                <Restart restartGame={this.restartGame} />
             </div>
         )
     }
 }
 
 export class PlayNine extends React.Component {
-    state = { list: [] }
+    //state = { list: [] }
 
     render() {
         return (
